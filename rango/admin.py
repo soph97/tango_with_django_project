9474d346513d06e64396d904e29cd02187dbb057
Sophie <2241442R@student.gla.ac.uk>
2018-01-31 10:51:27 +0000

    Seventh commit

--- rango/admin.py
+++ rango/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
+
+
 # Register your models here.
-admin.site.register(Category)
+
 admin.site.register(Page)
 
 
--- rango/urls.py
+++ rango/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$',views.index,name='index'),
-    url(r'about/', views.about, name = 'about')
+    url(r'^about/', views.about, name = 'about')
     ] 
 
 
