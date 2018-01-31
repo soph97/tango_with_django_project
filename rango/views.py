f2b0d2dd1f20201194b6a114bf54d16260415be3
Sophie <2241442R@student.gla.ac.uk>
2018-01-31 12:45:55 +0000

    eighth commit

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,24 @@ from rango.models import Page
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def show_category(request, category_name_slug):
+
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
+
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {'categories': category_list}
