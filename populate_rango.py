a20318b15a6437c920a31ff833d887022366b686
Sophie <2241442R@student.gla.ac.uk>
2018-02-05 13:01:07 +0000

    chap 7

--- populate_rango.py
+++ populate_rango.py
@@ -10,24 +10,24 @@ def populate():
 
     python_pages = [
         {"title": "Offical Python Tutorial",
-         "url" :"http://docs.python.org/2/tutorial/"},
+         "url" :"http://docs.python.org/2/tutorial/", "views": 60},
         {"title":"How to Think like a Computer Scientist",
-         "url":"http://www.greenteapress.com/thinkpython/"},
+         "url":"http://www.greenteapress.com/thinkpython/", "views": 80},
         {"title":"Learn Python in 10 Minutes",
-         "url":"http://www.korokithakis.net/tutorials/python/"} ]
+         "url":"http://www.korokithakis.net/tutorials/python/", "views": 90} ]
     django_pages = [
          {"title":"Official Django Tutorial",
-          "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+          "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/", "views": 65},
          {"title":"Django Rocks",
-          "url":"http://www.djangorocks.com/"},
+          "url":"http://www.djangorocks.com/", "views":110},
          {"title":"How to Tango with Django",
-          "url":"http://www.tangowithdjango.com/"} ]
+          "url":"http://www.tangowithdjango.com/", "views": 80} ]
 
     other_pages = [
         {"title":"Bottle",
-         "url":"http://bottlepy.org/docs/dev/"},
+         "url":"http://bottlepy.org/docs/dev/", "views": 70},
         {"title":"Flask",
-         "url":"http://flask.pocoo.org"} ]
+         "url":"http://flask.pocoo.org", "views": 90} ]
 
     cats = {"Python": {"pages": python_pages, "views":128, "likes":64},
             "Django": {"pages": django_pages, "views":32, "likes":64},
--- rango/views.py
+++ rango/views.py
@@ -1,33 +1,44 @@
-from rango.models import Category
-from rango.models import Page
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
+from rango.forms import CategoryForm
 
-def show_category(request, category_name_slug):
-
-    context_dict = {}
-    try:
-        category = Category.objects.get(slug=category_name_slug)
-
-        pages = Page.objects.filter(category=category)
-
-        context_dict['pages'] = pages
-
-        context_dict['category'] = category
-    except Category.DoesNotExist:
-
-        context_dict['category'] = None
-        context_dict['pages'] = None
-
-    return render(request, 'rango/category.html', context_dict)
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages':page_list}
+    
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
+    return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
     context_dict = {}
-    return render(request, 'rango/about.html', context=context_dict)
 
+    try:
+        category = Category.objetcs.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
 
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form}
